96ebb9e163004a1a2f2ba5c1d26c882c9a316673
kumaresh varsha <126232332+kumareshvarsha@users.noreply.github.com>
2023-03-13 22:20:53 +0530

    Add files via upload

---
+++ basic introductions/identifiers.js
@@ -0,0 +1,37 @@
+var a=10
+var b=40
+var ename="Varsha"
+var eID="101"
+var brand="kyra"
+var product_name="ladies top"
+var price=999;
+var ratings=4.2
+var discount=true;
+var availa='y'
+var stock;
+var size=[26,28,30,32,34]
+var emp={"id":"101", "name":"varsha"}
+var specification={"fits":"slim","size":"m"}
+console.log(a+b)
+console.log(ename)
+console.log(eID)
+console.log(brand)
+console.log(product_name)
+console.log(price)
+console.log(ratings)
+console.log(discount)
+console.log(availa)
+console.log(stock)
+console.log(size)
+console.log(emp)
+console.log(specification)
+console.log(typeof brand)
+console.log(typeof product_name)
+console.log(typeof price)
+console.log(typeof ratings)
+console.log(typeof discount)
+console.log(typeof availa)
+console.log(typeof stock)
+console.log(typeof size)
+console.log(typeof emp)
+console.log(typeof specification)
---
+++ basic introductions/one.js
@@ -0,0 +1,4 @@
+var a=20
+var b=50
+document.write(a+b)
+document.write("hello varsha")
